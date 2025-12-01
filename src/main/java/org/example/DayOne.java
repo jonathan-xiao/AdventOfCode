d80feac50474ab85323ff4b9b885e3d91ce84ec7
jonathan-xiao <jonathan.che.xiao@gmail.com>
2025-11-30 22:45:12 -0500

    Day Zero

---
+++ src/main/java/org/example/DayOne.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class DayOne {
+    static void main() {
+        System.out.println(dayOne(3));
+    }
+
+    public static int dayOne(int k) {
+        return k;
+    }
+}
