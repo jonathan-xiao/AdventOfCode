1ed3f6594868e0154493fb85ff5f8d781aca25ba
jonathan-xiao <jonathan.che.xiao@gmail.com>
2025-11-30 23:06:01 -0500

    Day Zero

--- src/main/java/org/example/Practice.java
+++ src/main/java/org/example/Practice.java
@@ -10,9 +10,8 @@ import java.io.BufferedReader;
 import java.io.*;
 import java.util.Collections;
 
-
-public class DayOne {
-    void main() {
+public class Practice {
+    public void main(String[] args) throws IOException {
         InputStream in = getClass().getClassLoader().getResourceAsStream("Practice.txt");
         List<String> lines = new BufferedReader(new InputStreamReader(in))
                 .lines().toList();
@@ -25,6 +24,16 @@ public class DayOne {
             leftList.add(Integer.parseInt(numbers[0]));
             rightList.add(Integer.parseInt(numbers[1]));
         }
-    }
 
+        Collections.sort(leftList);
+        Collections.sort(rightList);
+
+        int totalDistance = 0;
+        for (int i = 0; i < leftList.size(); i++) {
+            totalDistance += Math.abs(leftList.get(i) - rightList.get(i));
+        }
+
+        System.out.println("Total distance: " + totalDistance);
+    }
 }
+
